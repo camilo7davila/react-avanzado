a816a373e43c2c0636528772520504562fb785a5
Camilo Davila <camilo.davila@bci.cl>
2023-05-14 16:13:04 +0200

    Add | added partial control props

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { OnChangeArgs, Product } from '../interfaces/interfaces';
 
 interface UseProductArgs {
@@ -9,16 +9,8 @@ interface UseProductArgs {
 
 export const useProduct = ({ product, onChange, value = 0 }: UseProductArgs) => {
   const [counter, setCounter] = useState(value);
-  const isControlled = useRef(!!onChange);
 
   const increaseBy = (value: number) => {
-    if (isControlled.current) {
-      return onChange!({
-        count: value,
-        product
-      })
-    }
-    
     const newValue = Math.max(counter + value, 0)
     setCounter(newValue);
 
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -7,34 +7,33 @@ export const useShoppingCart = () => {
   const onProductCountChange = ({ count, product }: { count: number, product: Product }) => {
 
     setShoppingCart(oldState => {
-      const productInCart: ProductInCart = oldState[product.id] || { ...product, count: 0 };
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count
+      // const productInCart: ProductInCart = oldState[product.id] || { ...product, count: 0 };
+      // if (Math.max(productInCart.count + count, 0) > 0) {
+      //   productInCart.count += count
+      //   return {
+      //     ...oldState,
+      //     [product.id]: productInCart
+      //   }
+      // }
+      // //Borrar el producto
+      // const { [product.id]: toDelete, ...rest } = oldState;
+      // return {
+      //   ...rest
+      // }
+      // Para eliminar un objeto se desestructura y con el ...rest se deja lo que queremos
+      if (count === 0) {
+        const { [product.id]: toDelete, ...rest } = oldState;
         return {
-          ...oldState,
-          [product.id]: productInCart
+          ...rest
         }
       }
-      //Borrar el producto
-      const { [product.id]: toDelete, ...rest } = oldState;
+
       return {
-        ...rest
+        ...oldState,
+        [product.id]: { ...product, count }
       }
     })
 
-    // Para eliminar un objeto se desestructura y con el ...rest se deja lo que queremos
-    //   if (count === 0) {
-    //     const { [product.id]: toDelete, ...rest } = oldState;
-    //     return {
-    //       ...rest
-    //     }
-    //   }
-
-    //   return {
-    //     ...oldState,
-    //     [product.id]: { ...product, count }
-    //   }
-    // })
   }
 
   return {
